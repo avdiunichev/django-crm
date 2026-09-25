f1b70445bda80a57bea06c4e2227ff91e6cf6dc4
Artem Avdiunichev <avdiunichev@Artems-MacBook-Air.local>
2026-09-25 18:28:02 +0300

    Show all trips in driver profile

--- crm/views.py
+++ crm/views.py
@@ -10432,7 +10432,7 @@ class DriverDetailView(PersonalDataViewMixin, LoginRequiredMixin, DetailView):
                 "trailer",
             )
             .order_by("-transportation__planned_start_date", "-transportation__created_at")
-            .prefetch_related("transportation__stops", "transportation__parties__organization")[:12]
+            .prefetch_related("transportation__stops", "transportation__parties__organization")
         )
         for assignment in transportation_assignments:
             transportation = assignment.transportation
